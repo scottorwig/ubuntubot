5897915fadd4d9296678ad126bee958147f86bac
Scott Orwig <scott@orwig.us>
2012-02-22 14:22:34 -0500

    added configmanager.py

---
+++ configmanager.py
@@ -0,0 +1,34 @@
+# this code requires at config file with the format specified in the README file
+# specify the path here
+config_file_path = r'/home/orwig/Dropbox/lincoln_ubuntubot/ubuntubot.cfg'
+
+import ConfigParser
+
+
+def readconfig():
+    config = ConfigParser.ConfigParser()
+    config.read([config_file_path])    
+
+    gmail_user = config.get('gmail', 'user')
+    gmail_pwd = config.get('gmail', 'password')
+    prowl_address = config.get('prowl', 'email')    
+    follett_destination_directory = config.get('follett', 'destination_directory')
+    powerschool_server_root = config.get('powerschool', 'server_root')
+    powerschool_pw_page = config.get('powerschool', 'pw_page')
+    powerschool_user_password = config.get('powerschool', 'username_password')
+    path_to_host_file = config.get('ps1000', 'path_to_host_file')
+
+    config_values = {
+        'gmail_user': gmail_user,
+        'gmail_pwd': gmail_pwd,
+        'prowl_address': prowl_address,
+        'follett_destination_directory': follett_destination_directory,
+        'powerschool_server_root': powerschool_server_root,
+        'powerschool_pw_page': powerschool_pw_page,
+        'powerschool_user_password': powerschool_user_password,
+        'path_to_host_file': path_to_host_file,
+
+
+    }
+
+    return config_values
